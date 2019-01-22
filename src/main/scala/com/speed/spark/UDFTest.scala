8f6dbb6074b6c75e1fcde078ef2d45c8e15ddd48
liling <liling@163.com>
2019-01-22 11:23:41 +0800

    1

--- src/main/scala/com/speed/spark/UDFTest.scala
+++ src/main/scala/com/speed/spark/UDFTest.scala
@@ -17,7 +17,6 @@ object UDFTest {
   def main(args: Array[String]): Unit = {
 
 
-
     val sparkConf = new SparkConf().setAppName("sparksql").setMaster("local[*]")
 
 
@@ -105,13 +104,77 @@ class AverageSal extends UserDefinedAggregateFunction {
 
 
 //计算方差的UDAF
+//class AverageVariance extends UserDefinedAggregateFunction {
+//
+//  // 输入数据
+//  override def inputSchema: StructType = StructType(StructField("vs", DoubleType) :: Nil)
+//
+//  // 每一个分区中的 共享变量
+//  override def bufferSchema: StructType = StructType(StructField("vs_list", ArrayType(DoubleType)) :: Nil)
+//
+//  // 表示UDAF的输出类型
+//  override def dataType: DataType = DoubleType
+//
+//  // 表示如果有相同的输入是否会存在相同的输出，如果是则true
+//  override def deterministic: Boolean = true
+//
+//  // 初始化每一个分区中的 共享变量
+//  override def initialize(buffer: MutableAggregationBuffer): Unit = {
+//    buffer(0) = Nil
+//
+//  }
+//
+//  // 每一个分区中的每一条数据聚合的时候需要调用该方法
+//  override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
+//      println(input.getDouble(0))
+//      buffer(0) = buffer.getSeq[Double](0) :+ input.getDouble(0)
+//  }
+//
+//  // 将每一个分区的输出 合并 形成最后的数据
+//  override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
+//    val buf1 = buffer1.getSeq[Double](0)
+//    val buf2 = buffer2.getSeq[Double](0)
+//    buffer1(0) = buf1 ++ buf2
+//
+//  }
+//
+//  // 给出计算结果
+//  override def evaluate(buffer: Row): Any = {
+//
+//    var speed_list = scala.collection.mutable.ListBuffer[Double]();
+//
+//    for (item <- buffer.getSeq[Double](0)) speed_list += item
+//
+//    val sparkConf = new SparkConf().setAppName("sparksql").setMaster("local[*]")
+//
+//    val spark = SparkSession
+//      .builder()
+//      .config(sparkConf)
+//      .getOrCreate()
+//
+//    val sc = spark.sparkContext
+//
+//    val speed_rdd = sc.parallelize(speed_list);
+//    val data1 = speed_rdd.map(f => Vectors.dense(f));
+//    val stat = Statistics.colStats(data1);
+//    val variance = stat.variance.apply(0);
+//
+//    variance
+//
+//  }
+//
+//}
+
+
+//计算方差的UDAF
 class AverageVariance extends UserDefinedAggregateFunction {
 
+
   // 输入数据
-  override def inputSchema: StructType = StructType(StructField("vs", DoubleType) :: Nil)
+  override def inputSchema: StructType = StructType(StructField("vs", StringType) :: Nil)
 
   // 每一个分区中的 共享变量
-  override def bufferSchema: StructType = StructType(StructField("vs_list", ArrayType(DoubleType)) :: Nil)
+  override def bufferSchema: StructType = StructType(StructField("vs_list", StringType) :: Nil)
 
   // 表示UDAF的输出类型
   override def dataType: DataType = DoubleType
@@ -121,21 +184,24 @@ class AverageVariance extends UserDefinedAggregateFunction {
 
   // 初始化每一个分区中的 共享变量
   override def initialize(buffer: MutableAggregationBuffer): Unit = {
-    buffer(0) = Nil
+    buffer(0) = ""
 
   }
 
   // 每一个分区中的每一条数据聚合的时候需要调用该方法
   override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
+    if (buffer(0) == "") {
+      buffer(0) = input.getString(0)
+    } else {
+      buffer(0) = buffer.getString(0) + "," + input.getString(0)
+    }
 
-      buffer(0) = buffer.getSeq[Double](0) :+ input.getDouble(0)
   }
 
   // 将每一个分区的输出 合并 形成最后的数据
   override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
-    val buf1 = buffer1.getSeq[Double](0)
-    val buf2 = buffer2.getSeq[Double](0)
-    buffer1(0) = buf1 ++ buf2
+
+    buffer1(0) = buffer1.getString(0) + buffer2.getString(0)
 
   }
 
@@ -144,7 +210,14 @@ class AverageVariance extends UserDefinedAggregateFunction {
 
     var speed_list = scala.collection.mutable.ListBuffer[Double]();
 
-    for (item <- buffer.getSeq[Double](0)) speed_list += item
+    var item_double: Double = 0
+
+    for (item <- buffer.getString(0).split(",")) {
+
+      item_double = parseDouble(item).getOrElse(0)
+      speed_list += item_double
+
+    }
 
     val sparkConf = new SparkConf().setAppName("sparksql").setMaster("local[*]")
 
@@ -155,14 +228,23 @@ class AverageVariance extends UserDefinedAggregateFunction {
 
     val sc = spark.sparkContext
 
-    val speed_rdd = sc.parallelize(speed_list);
-    val data1 = speed_rdd.map(f => Vectors.dense(f));
-    val stat = Statistics.colStats(data1);
-    val variance = stat.variance.apply(0);
+    if (speed_list.length > 0) {
+      val speed_rdd = sc.parallelize(speed_list);
+      val data1 = speed_rdd.map(f => Vectors.dense(f));
+      val stat = Statistics.colStats(data1);
+      val variance = stat.variance.apply(0);
 
-    variance
+      variance
+    }else{
+      0.0
+    }
+  }
 
+  //将String转Double
+  def parseDouble(s: String): Option[Double] = try {
+    Some(s.toDouble)
+  } catch {
+    case _ => None
   }
 
 }
-
