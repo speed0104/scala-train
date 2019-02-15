c04340ea3c1015cfc2b98203ef19552b975ad89e
speed0104 <speed0104@163.com>
2019-02-15 17:00:49 +0800

    1

--- src/main/scala/com/speed/spark/wordcount.scala
+++ src/main/scala/com/speed/spark/wordcount.scala
@@ -9,12 +9,12 @@ object wordcount {
     val conf = new SparkConf().setAppName("wordcount").setMaster("local[1]")
     val sc =new SparkContext(conf)
 
-    val lines = sc.textFile("spark.txt",1)
-    val words = lines.flatMap{ line => line.split("")}
+    val lines = sc.textFile("words.txt",1)
+    val words = lines.flatMap{ line => line.split(" ")}
     val pairs = words.map{word => (word,1)}
-    val wordcounts = pairs.reduceByKey{_+_}
+    val wordcounts = pairs.reduceByKey{ _ + _ }
     val sortedcordcount = wordcounts.sortBy(x=>x._2,false)
-    sortedcordcount.foreach{ x=> println(x._1 + "appears" + x._2 + " times.")}
+    sortedcordcount.foreach{ x=> println(x._1 + " appears " + x._2 + " times.")}
     sortedcordcount.cache()
     sortedcordcount.count()
 
