f72cab3aa404b781aaed5a5fdc6a1ef81e2bb46b
liling <liling@163.com>
2019-03-26 14:04:12 +0800

    1

--- src/main/scala/com/speed/spark/df/CreateDFFromHive.scala
+++ src/main/scala/com/speed/spark/df/CreateDFFromHive.scala
@@ -15,29 +15,28 @@ object CreateDFFromHive {
      * HiveContext是SQLContext的子类。
      */
     val hiveContext = new HiveContext(sc)
-    hiveContext.sql("use test")
-    hiveContext.sql("drop table if exists student_infos")
-    hiveContext.sql("create table if not exists student_infos (name string,age int) row format  delimited fields terminated by '\t' stored as textfile")
+   hiveContext.sql("use test")
+/*     hiveContext.sql("drop table if exists student_infos")
+    hiveContext.sql("create table if not exists student_infos (name string,age int) row format  delimited fields terminated by '\t'")
+//    val df1 = hiveContext.sql("select * from drv_info limit 10")
+//    df1.show()
 
-    val df1 = hiveContext.sql("select * from drv_info limit 10")
-    df1.show()
 
+    hiveContext.sql("load data local inpath 'data/student_infos' into table student_infos")
 
-    //    hiveContext.sql("load data local inpath 'data/student_infos.txt' into table student_infos")
-    //
-    //    val df = hiveContext.read.json("data/employees.json")
-    //    df.write.mode(SaveMode.Overwrite).saveAsTable("good_student_infos")
-//
-//    hiveContext.sql("drop table if exists student_scores")
-//    hiveContext.sql("create table if not exists student_scores (name string,score int) row format delimited fields terminated by '\t'")
-//    hiveContext.sql("load data local inpath 'data/student_scores' into table student_scores")
-//
-//    val df = hiveContext.sql("select si.name,si.age,ss.score from student_infos si,student_scores ss where si.name = ss.name")
-//    hiveContext.sql("drop table if exists good_student_infos")
-//    /**
-//     * 将结果写入到hive表中
-//     */
-//    df.write.mode(SaveMode.Overwrite).saveAsTable("good_student_infos")
+    hiveContext.sql("drop table if exists student_scores")
+    hiveContext.sql("create table if not exists student_scores (name string,score int) row format delimited fields terminated by '\t'")
+    hiveContext.sql("load data local inpath 'data/student_scores' into table student_scores")
+
+    val df = hiveContext.sql("select si.name,si.age,ss.score from student_infos si,student_scores ss where si.name = ss.name")
+    hiveContext.sql("drop table if exists good_student_infos")
+    /**
+     * 将结果写入到hive表中
+     */
+    df.write.mode(SaveMode.Overwrite).saveAsTable("good_student_infos")*/
+
+    val df = hiveContext.read.json("data/employees.json")
+    df.write.mode(SaveMode.Overwrite).saveAsTable("employees")
     
     sc.stop()
   }
